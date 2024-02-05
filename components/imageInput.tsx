095bd2fa70ab65862c3cdd6b4994786adcb7a5ce
kazutan1230 <kazuki.nagai@bnt.benextgroup.jp>
2024-02-05 09:44:40 +0000

    Fixing the upload page error, for forwardRef render functions

--- components/imageInput.tsx
+++ components/imageInput.tsx
@@ -12,7 +12,7 @@ type Props = ComponentPropsWithRef<'input'> & {
   >
 
 export const ImageInput: FC<Props> = forwardRef(
-  ({ onChange, id, fileInputRef }) => {
+  ({ onChange, id }, fileInputRef) => {
     return (
       <input
         ref={fileInputRef}
