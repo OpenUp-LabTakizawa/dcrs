c2958555b19125fe122fadb008eb66b0d7afdca6
HaradaTakayuki <takayuki.harada@bnt.benextgroup.jp>
2024-03-08 01:11:20 +0000

    テストページ変更の同期

--- app/upload/confirm/page.tsx
+++ app/upload/confirm/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link'
 
 export default function Upload() {
   const localFormData = JSON.parse(localStorage.getItem('FormData') || '{}')
@@ -22,20 +23,20 @@ export default function Upload() {
         </p>
       </div>
       <div className="mt-8 space-y-6">
-        <a
+        <Link
           href="/"
           className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           [入力事項]を送信する。
-        </a>
+        </Link>
       </div>
       <div className="mt-8 space-y-6">
-        <a
+        <Link
           href="/upload"
           className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           [入力事項]を修正する。
-        </a>
+        </Link>
       </div>
     </main>
   )
