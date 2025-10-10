af240eaa8914d91aeabde143ef09d6673ec24310
Akio Muto <akio_muto@toyota-energy.co.jp>
2025-10-10 09:14:50 +0900

    chore: remove cacheComponents setting

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   experimental: {
-    cacheComponents: true,
     inlineCss: true,
     isrFlushToDisk: false,
     // Disable react compiler due to react hook form
